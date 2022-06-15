8d32dd1e40b6aca58b58ea987c817621ea01d037
Ali Ahmad <aliahmad4585256@gmail.com>
2022-06-16 02:13:31 +0500

    change the menefist file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
     <title>Skill Challenge</title>
 
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
